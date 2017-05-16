ee6fa5edae928af32d3844bb2fedd86c21d1fd0a
bnapky <bnapky@prosoftnearshore.com>
2017-05-15 23:07:03 -0600

    trigger

---
+++ front-end/src/views/trigger/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class TriggerView extends Component {
+    state = {  }
+    render() {
+        return (
+            <h2>Trigger</h2>
+        );
+    }
+}
+
+export default TriggerView;
